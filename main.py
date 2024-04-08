4c2698bdc9b91d17ead111a013866be26a5de18b
Kodee <kodeekong@yahoo.com>
2024-04-08 10:11:05 -0400

    more

--- main.py
+++ main.py
@@ -13,20 +13,33 @@ def home():
     return render_template('index.html')
 
 
-# @app.route('/account_view')
+@app.route('/account_view')
+def home():
+    return render_template('show_accounts.html')
+    conn.execute(text("select * from accounts"), request.form)
+    conn.commit()
+    return render_template('show_accounts.html')
 
 
 @app.route('/create_account', methods = ['POST'])
 def create_account():
-    conn.execute(text("INSERT INTO accounts VALUES (:type, :name, :email, :username, :password)"), request.form)
+    conn.execute(text("INSERT INTO accounts VALUES (:id, :username, :email, :password)"), request.form)
     conn.commit()
-    return render_template('register.html')
+    return render_template('create_account.html')
 
 
-@app.route('/create_boat', methods=['GET'])
-def create_get_request():
-    return  render_template('register.html')
+@app.route('/filter', methods=['GET'])
+def filter():
+    return render_template('filter.html')
 
+
+@app.route('/filter', methods = ['POST'])
+def filter_accounts():
+    x = request.form['id']
+    account = conn.execute(text(f'select * from accounts id type = {x}'))
+    conn.execute(text(f"select * from boats where ID = {x}"), request.form)
+    conn.commit()
+    return render_template('filter.html', boat_info=boat)
 
 
 if __name__ == '__main__':
